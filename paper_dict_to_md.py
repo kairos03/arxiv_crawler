fe4a399cbf90a7b29390d564089c828c7640c847
karios03 <kairos9603@gmail.com>
2017-10-26 10:19:48 +0200

    add write header option

--- paper_dict_to_md.py
+++ paper_dict_to_md.py
@@ -1,20 +1,21 @@
 
-def paper_dict_to_md(paper_bunch):
+def paper_dict_to_md(paper_bunch, header_out=True):
     date = paper_bunch['date']
     file_name = date + "-daily-DL-paper.md"
     f = open(file_name, "w")
 
-    header = '---\n' \
-             'layout: post\n' \
-             'title: \"' + date + ' 오늘의 페이퍼\"\n' \
-             'subtitle: 오늘의 딥러닝 논문\n' \
-             'date: ' + date + '\n' \
-             'categories: Machine-learning\n' \
-             'cover: https://user-images.githubusercontent.com/6357456/30470074-9b04e2fc-99f3-11e7-9c89-869dc06cc8f3.png\n' \
-             'tag:    [AI, machine-learning, deep-learning, paper, trend]\n' \
-             '---\n\n'
+    if header_out:
+        header = '---\n' \
+                 'layout: post\n' \
+                 'title: \"' + date + ' 오늘의 페이퍼\"\n' \
+                 'subtitle: 오늘의 딥러닝 논문\n' \
+                 'date: ' + date + '\n' \
+                 'categories: Machine-learning\n' \
+                 'cover: https://user-images.githubusercontent.com/6357456/30470074-9b04e2fc-99f3-11e7-9c89-869dc06cc8f3.png\n' \
+                 'tag:    [AI, machine-learning, deep-learning, paper, trend]\n' \
+                 '---\n\n'
 
-    f.write(header)
+        f.write(header)
 
     for paper_list in paper_bunch['paper_list']:
 
